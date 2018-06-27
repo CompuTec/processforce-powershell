73e366c1cba4db751009aead3f16db37279d9b09
Maciej Pieprzyk <maciejp@ct.local>
2018-06-27 15:07:47 +0200

    Logger

--- Performance/Import.ps1
+++ Performance/Import.ps1
@@ -1,11 +1,14 @@
 using module .\lib\CTLogger.psm1;
 Clear-Host
 [System.Reflection.Assembly]::LoadWithPartialName("CompuTec.ProcessForce.API")
-
+$RESULT_FILE = $PSScriptRoot + "\Results.csv";
+[CTLogger] $logJobs = New-Object CTLogger ('DI', 'Import', $RESULT_FILE)
 #region connection
 [xml] $connectionConfigXml = Get-Content -Encoding UTF8 .\conf\Connection.xml
 $xmlConnection = $connectionConfigXml.SelectSingleNode("/CT_CONFIG/Connection");
-    
+
+$logJobs.startSubtask('Import');
+$logJobs.startSubtask('Connection');
 $pfcCompany = [CompuTec.ProcessForce.API.ProcessForceCompanyInitializator]::CreateCompany();
 $pfcCompany.LicenseServer = $xmlConnection.LicenseServer;
 $pfcCompany.SQLServer = $xmlConnection.SQLServer;
@@ -28,7 +31,7 @@ try {
 catch {
     #Show error messages & stop the script
     write-host "Connection Failure: " -backgroundcolor red -foregroundcolor white $_.Exception.Message
-    
+    $logJobs.endSubtask('Connection','F',$_.Exception.Message);
     write-host "LicenseServer:" $pfcCompany.LicenseServer
     write-host "SQLServer:" $pfcCompany.SQLServer
     write-host "DbServerType:" $pfcCompany.DbServerType
@@ -39,14 +42,16 @@ catch {
 #If company is not connected - stops the script
 if (-not $pfcCompany.IsConnected) {
     write-host -backgroundcolor yellow -foregroundcolor black "Company is not connected"
+    $logJobs.endSubtask('Connection','F','Company is not connected');
     return 
 }
-
+$logJobs.endSubtask('Connection','S','');
 $sapCompany = $pfcCompany.SapCompany;
 #endregion
 
 
 function importIMD($sapCompany) {
+    [CTLogger] $logIMD = New-Object CTLogger ('DI', 'Import Item Master Data', $RESULT_FILE)
     #region import of Item Master Data
     [xml] $IMDConfigXml = Get-Content -Encoding UTF8 .\conf\ItemMasterData.xml
 
@@ -56,10 +61,10 @@ function importIMD($sapCompany) {
     $itemCodeLength = ([string]$numberOfItems).Length;
     $itemPrefix = [string] $xmlItems.Prefix
     $warehouseCode = [string] $xmlItems.WarehouseCode
-
+    
     for ($i = 0; $i -lt $numberOfItems; $i++) {
         try {
-       
+            $logIMD.startSubtask('Preparing to add Item Master Data to SAP');
             $sapIMD = $sapCompany.GetBusinessObject([SAPbobsCOM.BoObjectTypes]::oItems);
         
             $ItemCode = $itemPrefix + ([string]$i).PadLeft($itemCodeLength, '0');
@@ -67,7 +72,7 @@ function importIMD($sapCompany) {
             $retValue = $sapIMD.GetByKey($ItemCode)
 
             if ($retValue -eq $true) {
-                write-host 'Item Already Exists';
+                $logIMD.endSubtask('Preparing to add Item Master Data to SAP','S','Item Already Exists');
                 continue;
             }
 
@@ -75,18 +80,28 @@ function importIMD($sapCompany) {
             $sapIMD.ItemName = $ItemCode;
             $sapIMD.WhsInfo.WarehouseCode = $warehouseCode;
             $sapIMD.DefaultWarehouse = $warehouseCode;
-
+            $logIMD.endSubtask('Preparing to add Item Master Data to SAP','S','');
+            $logIMD.startSubtask('Adding Item Master Data to SAP');
             $message = $sapIMD.Add();
+            
             if ($message -lt 0) {
                 $err = $sapCompany.GetLastErrorDescription();
                 Throw [System.Exception] ($err);
             }
+            $logIMD.endSubtask('Adding Item Master Data to SAP','S','');
         }
         Catch {
             $err = $_.Exception.Message;
-            Write-Host -BackgroundColor DarkRed -ForegroundColor White $content;
+            $logIMD.endSubtask('Adding Item Master Data to SAP','F',$err);
             continue;
         }
     }
     #endregion
 }
+$logJobs.startSubtask('Import IMD');
+importIMD $sapCompany
+$logJobs.endSubtask('Import IMD','S','');
+
+
+
+$logJobs.endSubtask('Import','S','');
--- Performance/lib/CTTimer.psm1
+++ Performance/lib/CTTimer.psm1
@@ -3,13 +3,17 @@ Class CTTimer {
     [DateTime] $endDateTime;
     [DateTime] $prevStepDateTime;
     [DateTime] $currentDateTime;
-    
+    [bool] $started;
     CTTimer() {
         $this.startDateTime = Get-Date;
         $this.prevStepDateTime = $this.startDateTime;
+        $this.started = $true;
     }
 
     [double] round(){
+        if($this.started -eq $false){
+            $this.restart();
+        }
         $this.currentDateTime = Get-Date;
         $seconds = ($this.currentDateTime-$this.prevStepDateTime).TotalSeconds;
         $this.prevStepDateTime = $this.currentDateTime;
@@ -17,18 +21,24 @@ Class CTTimer {
     }
 
     [double] totalSeconds(){
-        $this.endDateTime =  Get-Date;
-        $seconds = ($this.startDateTime-$this.endDateTime).TotalSeconds;
+        if($this.started -eq $false){
+            $end = $this.endDateTime;
+        } else {
+            $end = Get-Date;
+        }
+        $seconds = ($end - $this.startDateTime).TotalSeconds;
         return $seconds;
     }
 
     [void] stop(){
         $this.endDateTime =  Get-Date;
+        $this.started = $false;
     }
 
     [void] restart() {
         $this.startDateTime = Get-Date;
         $this.prevStepDateTime = $this.startDateTime;
+        $this.started = $true;
     }
 
 
