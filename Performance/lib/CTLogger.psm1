187d8e7244442c2bbfa5e18732844ca60a4d099c
maciejp <maciej.pieprzyk@computec.pl>
2019-09-19 16:42:51 +0200

    Form Maximized, Regular, refactoring

--- Performance/lib/CTLogger.psm1
+++ Performance/lib/CTLogger.psm1
@@ -16,11 +16,19 @@ Class CTLogger {
         $this.prepareLogFile()
     }
 
-    [void] startSubtask($subtaskName){
+	[void] startSubtask($subtaskName){
+        $this.startSubtask($subtaskName, '');
+    }
+
+	<# $subtaskName [string] name of subtask
+		$additionalInfo [string] additional information
+	#>
+    [void] startSubtask($subtaskName, $additionalInfo){
         if($this.tasks.ContainsKey($subtaskName) -eq $false) {
             $this.tasks.Add($subtaskName,
                 [psobject]@{
-                   timer = New-Object CTTimer
+				   timer = New-Object CTTimer
+				   additionalInfo = [string]$additionalInfo
                 });
         } else {
             Throw [System.Exception]("subtask already started:" + $subtaskName);
@@ -42,8 +50,8 @@ Class CTLogger {
     }
 
     [void] logToFile($subtask){
-        $content = [string]::Format("{0}; {1}; {2}; {3}; {4}; {5}; {6}; {7}",
-        $this.connectionType, $this.task,$subtask.name, $subtask.timer.startDateTime, $subtask.timer.endDateTime,  $subtask.timer.totalSeconds(), $subtask.status, $subtask.message);
+        $content = [string]::Format("{0}; {1}; {2}; {3}; {4}; {5}; {6}; {7}, {8}",
+        $this.connectionType, $this.task,$subtask.name, $subtask.timer.startDateTime, $subtask.timer.endDateTime,  $subtask.timer.totalSeconds(), $subtask.status, $subtask.additionalInfo, $subtask.message);
         Add-Content -path $this.fileName $content;
     }
 
@@ -52,7 +60,7 @@ Class CTLogger {
             return;
         }
 
-        $content = [string]::Format("Connection Type; Task; Subtask; Start Time; End Time; Duration [s]; Task Status; Remarks");
+        $content = [string]::Format("Connection Type; Task; Subtask; Start Time; End Time; Duration [s]; Task Status; Remarks; Addittional Info");
         Add-Content -Path $this.fileName $content;
         
     }
